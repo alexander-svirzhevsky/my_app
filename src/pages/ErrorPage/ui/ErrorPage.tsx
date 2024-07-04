41f3b90db78ad1c095d0220ca8ffb3ba3c4b4b63
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2024-07-04 13:12:10 +0300

    added error

--- src/pages/ErrorPage/ui/ErrorPage.tsx
+++ src/pages/ErrorPage/ui/ErrorPage.tsx
@@ -14,6 +14,8 @@ export const ErrorPage = ({ className }: ErrorPageProps) => {
         location.reload();
     };
 
+    const b: number = '10'
+
     return (
         <div className={classNames(cn['errorPage'], {}, [className])}>
             <span>{t('something_went_wrong')}</span>
